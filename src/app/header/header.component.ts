bf4975933a6f86d9f1186eefd1cefe7790cada68
Muhammad Fauzi <fauzi87sg@gmail.com>
2018-06-14 13:47:51 +0800

    angular: eventemitter

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -10,13 +10,35 @@ import { UserService } from '../shared/user.service'
 })
 export class HeaderComponent implements OnInit {
   isLoggedIn: boolean = false
+  name: string
+  uid: string
+  email: string
 
   constructor(private userService: UserService) { }
 
   ngOnInit() {
+    // can be deleted
+    this.userService.statusChange.subscribe(userData => {
+      if (userData) {
+        this.name = userData.name
+        this.email = userData.email
+        this.uid = userData.uid
+      } else {
+        this.name = null
+        this.email = null
+        this.uid = null
+      }
+    })
+
     firebase.auth().onAuthStateChanged(userData => {
       // we are logged in
       if (userData && userData.emailVerified) {
+        const user = this.userService.getProfile()
+        if (user) {
+        this.name = user.name
+        this.email = user.email
+        this.uid = user.uid
+        }
         this.isLoggedIn = true
       } else {
         this.isLoggedIn = false
--- src/app/shared/user.service.ts
+++ src/app/shared/user.service.ts
@@ -1,11 +1,24 @@
+import { EventEmitter } from '@angular/core'
+
 export class UserService {
+  // can be deleted
+  statusChange: any = new EventEmitter<any>()
 
   public set(userFromDatabase) {
     localStorage.setItem('user', JSON.stringify(userFromDatabase))
+    // can be deleted
+    this.statusChange.emit(userFromDatabase)
+  }
+
+  public getProfile() {
+    const user = localStorage.getItem('user')
+    return JSON.parse(user)
   }
 
   public destroy() {
     localStorage.removeItem('user')
+    // can be deleted
+    this.statusChange.emit(null)
   }
 
 }
