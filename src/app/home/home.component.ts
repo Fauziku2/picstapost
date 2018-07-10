35950eadacaa58f508ba898d8e84d91300943bc8
Muhammad Fauzi <fauzi87sg@gmail.com>
2018-07-10 16:51:52 +0800

    change home button if user is logged in

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
+import * as firebase from 'firebase/app'
+import 'firebase/auth'
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
+
 export class HomeComponent implements OnInit {
+  isLoggenIn: boolean = false
 
   constructor() { }
 
   ngOnInit() {
+    if (firebase.auth().currentUser) {
+      this.isLoggenIn = true
+    } else {
+      this.isLoggenIn = false
+    }
   }
 
 }
